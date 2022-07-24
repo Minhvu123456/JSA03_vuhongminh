035719b640c4b6a60183596717187961d9600420
Minhvu123456 <87233069+Minhvu123456@users.noreply.github.com>
2022-07-24 16:05:47 +0700

    baikiemtra

---
+++ btvn1/session4/index.js
@@ -0,0 +1,25 @@
+// Đối tượng (object): Array, Object, Null, Date, Function, ...
+// Nguyên thủy (native): String, Number, Boolean,...
+
+// -- Native
+// - String: 'Lớp JSA03'
+// - Number: 1-09 
+// - Boolean: true | false
+
+// -- Object
+// - Array: [ 1,2, 'a', 'b', 'c' ]
+// - Object: {
+//     'a': 1,
+//     'b': 2   
+// }
+// - Date: new Date()
+
+
+
+function sum(a,b){
+    let sum = a*b;
+    console.log(sum);
+    return sum;
+}
+
+sum(3,5)
